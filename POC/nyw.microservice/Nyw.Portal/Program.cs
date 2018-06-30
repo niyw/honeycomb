27b74954e051d5fc96f36300567027d004bb1da8
niyw <nyw@live.cn>
2018-06-30 20:59:51 +0800

    Nyw.Protal Upgrade to .NETCore 2.1

--- POC/nyw.microservice/Nyw.Portal/Program.cs
+++ POC/nyw.microservice/Nyw.Portal/Program.cs
@@ -8,18 +8,18 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace Nyw.Portal
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            BuildWebHost(args).Run();
+namespace Nyw.Portal {
+    public class Program {
+        public static void Main(string[] args) {
+            CreateWebHostBuilder(args).Build().Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
                 .Build();
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+            .UseStartup<Startup>();
     }
 }
