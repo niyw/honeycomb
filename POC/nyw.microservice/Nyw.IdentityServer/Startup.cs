689785bcec956b3ae6861c5f22fef73a6ccb182c
niyw <nyw@live.cn>
2018-11-13 17:28:18 +0800

    refactor identity db code

--- POC/nyw.microservice/Nyw.IdentityServer/Startup.cs
+++ POC/nyw.microservice/Nyw.IdentityServer/Startup.cs
@@ -5,6 +5,10 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using System.Reflection;
 using Microsoft.Extensions.Configuration;
+using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.Models;
+using IdentityServer4.EntityFramework.Mappers;
+using System.Linq;
 
 namespace Nyw.IdentityServer {
     public class Startup {
@@ -16,41 +20,67 @@ namespace Nyw.IdentityServer {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services) {
-            var idpDbConnStr = Configuration.GetConnectionString("IdpDbConnStr");
-            var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
-
-            // configure identity server with in-memory stores, keys, clients and scopes
-            services.AddIdentityServer()
-                .AddDeveloperSigningCredential()
-                 .AddUserModel()
-                // this adds the config data from DB (clients, resources)
-                .AddConfigurationStore(options => {
-                    options.ConfigureDbContext = builder =>
-                        builder.UseSqlServer(idpDbConnStr,
-                            sql => sql.MigrationsAssembly(migrationsAssembly));
-                })
-                // this adds the operational data from DB (codes, tokens, consents)
-                .AddOperationalStore(options => {
-                    options.ConfigureDbContext = builder =>
-                        builder.UseSqlServer(idpDbConnStr,
-                            sql => sql.MigrationsAssembly(migrationsAssembly));
-
-                    // this enables automatic token cleanup. this is optional.
-                    options.EnableTokenCleanup = true;
-                    options.TokenCleanupInterval = 30;
-                });
-
+            ConfigureIdentityServerServices(services);
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
             if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
+                InitializeIdentityServerDatabase(app);
             }
+            app.UseMvc();
+        }
+        private void ConfigureIdentityServerServices(IServiceCollection services) {
+            var idpDbConnStr = Configuration.GetConnectionString("IdpDbConnStr");
+            var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+            services.AddIdentityServer()
+                .AddDeveloperSigningCredential()
+                .AddUserModel()
+                .AddConfigurationStore(options => {
+                    options.ConfigureDbContext = builder =>
+                        builder.UseSqlServer(idpDbConnStr,
+                            sql => sql.MigrationsAssembly(migrationsAssembly));
+                })
+                .AddOperationalStore(options => {
+                    options.ConfigureDbContext = builder =>
+                        builder.UseSqlServer(idpDbConnStr,
+                            sql => sql.MigrationsAssembly(migrationsAssembly));
+                    options.EnableTokenCleanup = true;
+                    options.TokenCleanupInterval = 30;
+                });
 
-            app.Run(async (context) => {
-                await context.Response.WriteAsync("Hello World!");
-            });
+        }
+        private void InitializeIdentityServerDatabase(IApplicationBuilder app) {
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope()) {
+                serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+                var context = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+                context.Database.Migrate();
+                if (!context.Clients.Any()) {
+                    var client1 = new IdentityServer4.Models.Client {
+                        ClientId = "demo.client1",
+                        AllowedGrantTypes = GrantTypes.ClientCredentials,
+                        ClientSecrets = {
+                            new IdentityServer4.Models.Secret("secret".Sha256())
+                        },
+                        AllowedScopes = { "demo.api1" },
+                        ClientName = "Demo Client1"
+                    };
+                    context.Clients.Add(client1.ToEntity());
+                    context.SaveChanges();
+                }
+
+                if (!context.ApiResources.Any()) {
+                    var api1 = new IdentityServer4.Models.ApiResource {
+                        Name = "demo.api1",
+                        DisplayName = "demo api",
+                        Description = "this is just for demo"
+                    };
+                    context.ApiResources.Add(api1.ToEntity());
+                    context.SaveChanges();
+                }
+            }
         }
     }
 }
