df50b5817a810be3957e825d5be1487595ed3d3d
niyw <nyw@live.cn>
2018-11-14 17:20:29 +0800

    add nyw.configuration.sqlserver project craft

---
+++ POC/nyw.microservice/Nyw.Configuration.SqlServer/AppConfigDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nyw.Configuration.SqlServer {
+    public class AppConfigDbContext : DbContext {
+        public AppConfigDbContext(DbContextOptions options) : base(options) {
+        }
+
+        public DbSet<AppKeyValue> KeyValues { get; set; }
+    }
+}
---
+++ POC/nyw.microservice/Nyw.Configuration.SqlServer/AppKeyValue.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Nyw.Configuration.SqlServer {
+    public class AppKeyValue {
+        public string Id { get; set; }
+        public string Value { get; set; }
+    }
+}
