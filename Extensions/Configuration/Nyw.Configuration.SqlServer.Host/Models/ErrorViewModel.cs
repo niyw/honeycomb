efaf22c63e4504e8ff5ba60bdcdd1fbcff829226
niyw <nyw@live.cn>
2018-11-17 22:23:18 +0800

    remove Nyw.configuration.sqlserver project

--- Extensions/Configuration/Nyw.Configuration.SqlServer.Host/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace Nyw.Configuration.SqlServer.Host.Models {
-    public class ErrorViewModel {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
